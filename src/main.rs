63fc829be9ee347e5ff7a8dcdf28f21f608be561
Guzman Zugnoni <gooseiman@protonmail.com>
2024-01-01 21:28:27 -0300

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-#[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/", get(index));
+use axum::{routing::get, Router};
+use color_eyre::Result;
+use std::net::SocketAddr;
 
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get(index));
+    let addr: SocketAddr = "0.0.0.0:3779".parse().unwrap();
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn index() -> &'static str {
+    "Hello World"
 }
