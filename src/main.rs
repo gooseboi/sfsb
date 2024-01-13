7959065fd65342fb9b169c8be7057dc909c8f3e6
Guzman Zugnoni <gooseiman@protonmail.com>
2024-01-13 19:01:17 -0300

    serve files

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use askama_axum::IntoResponse;
 use axum::{
     body::Body,
     extract::{self, Query, State},
-    http::{Response, StatusCode},
+    http::{HeaderMap, Response, StatusCode},
     response::Redirect,
     routing::get,
     Router,
@@ -148,9 +148,130 @@ async fn serve_path(
     fetch_path(&path, Arc::clone(&state.cache), query).await
 }
 
+fn content_type_from_extension(ext: Option<&str>) -> &str {
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+    let Some(ext) = ext else {
+        // FIXME: text/plain or application/octet-stream?
+        return "text/plain";
+    };
+    match ext {
+        ".aac" => "audio/aac",
+        ".abw" => "application/x-abiword",
+        ".apng" => "image/apng",
+        ".arc" => "application/x-freearc",
+        ".avif" => "image/avif",
+        ".avi" => "video/x-msvideo",
+        ".azw" => "application/vnd.amazon.ebook",
+        ".bin" => "application/octet-stream",
+        ".bmp" => "image/bmp",
+        ".bz" => "application/x-bzip",
+        ".bz2" => "application/x-bzip2",
+        ".cda" => "application/x-cdf",
+        ".csh" => "application/x-csh",
+        ".css" => "text/css",
+        ".csv" => "text/csv",
+        ".doc" => "application/msword",
+        ".docx" => "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        ".eot" => "application/vnd.ms-fontobject",
+        ".epub" => "application/epub+zip",
+        ".gz" => "application/gzip",
+        ".gif" => "image/gif",
+        ".htm" | ".html" => "text/html",
+        ".ico" => "image/vnd.microsoft.icon",
+        ".ics" => "text/calendar",
+        ".jar" => "application/java-archive",
+        ".jpeg" | ".jpg" => "image/jpeg",
+        ".js" => "text/javascript",
+        ".json" => "application/json",
+        ".jsonld" => "application/ld+json",
+        ".mid," => "audio/midi",
+        ".mjs" => "text/javascript",
+        ".mp3" => "audio/mpeg",
+        ".mp4" => "video/mp4",
+        ".mpeg" => "video/mpeg",
+        ".mpkg" => "application/vnd.apple.installer+xml",
+        ".odp" => "application/vnd.oasis.opendocument.presentation",
+        ".ods" => "application/vnd.oasis.opendocument.spreadsheet",
+        ".odt" => "application/vnd.oasis.opendocument.text",
+        ".oga" => "audio/ogg",
+        ".ogv" => "video/ogg",
+        ".ogx" => "application/ogg",
+        ".opus" => "audio/opus",
+        ".otf" => "font/otf",
+        ".png" => "image/png",
+        ".pdf" => "application/pdf",
+        ".php" => "application/x-httpd-php",
+        ".ppt" => "application/vnd.ms-powerpoint",
+        ".pptx" => "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+        ".rar" => "application/vnd.rar",
+        ".rtf" => "application/rtf",
+        ".sh" => "application/x-sh",
+        ".svg" => "image/svg+xml",
+        ".tar" => "application/x-tar",
+        ".tif" | ".tiff" => "image/tiff",
+        ".ts" => "video/mp2t",
+        ".ttf" => "font/ttf",
+        ".txt" => "text/plain",
+        ".vsd" => "application/vnd.visio",
+        ".wav" => "audio/wav",
+        ".weba" => "audio/webm",
+        ".webm" => "video/webm",
+        ".webp" => "image/webp",
+        ".woff" => "font/woff",
+        ".woff2" => "font/woff2",
+        ".xhtml" => "application/xhtml+xml",
+        ".xls" => "application/vnd.ms-excel",
+        ".xlsx" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        ".xml" => "application/xml",
+        ".xul" => "application/vnd.mozilla.xul+xml",
+        ".zip" => "application/zip",
+        ".3gp" => "video/3gpp",
+        ".3g2" => "video/3gpp2",
+        ".7z" => "application/x-7z-compressed",
+        // FIXME: Same as above
+        _ => "text/plain",
+    }
+}
+
 async fn dl_path(
     extract::Path(path): extract::Path<PathBuf>,
     State(state): State<AppState>,
-) -> impl IntoResponse {
+    headers: HeaderMap,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
     info!("Downloding [{p}]", p = path.to_string_lossy());
+    let data_dir = &state.data_dir;
+    let file_path = {
+        let mut p = data_dir.to_path_buf();
+        p.push(&path);
+        p
+    };
+
+    let metadata = tokio::fs::metadata(&file_path)
+        .await
+        .map_err(|e| (StatusCode::NOT_FOUND, e.to_string()))?;
+    if metadata.is_dir() {
+        return Err((
+            StatusCode::UNSUPPORTED_MEDIA_TYPE,
+            format!("TODO: Cannot download folders yet: requested {path:?}"),
+        ));
+    }
+    let len = metadata.len();
+    let ext = path.extension().map(|s| s.to_str().unwrap());
+    let ext = content_type_from_extension(ext);
+    let file = tokio::fs::File::open(file_path)
+        .await
+        .map_err(|e| (StatusCode::NOT_FOUND, e.to_string()))?;
+    let stream = tokio_util::io::ReaderStream::new(file);
+    let stream = axum::body::Body::from_stream(stream);
+
+    Response::builder()
+        .header("Accept-Ranges", "bytes")
+        .header("Content-Length", len)
+        .header("Content-Type", ext)
+        .header(
+            "Content-Disposition",
+            format!("attachment; filename=\"{p}\"", p = path.to_string_lossy()),
+        )
+        .body(stream)
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
 }
