6cde0ac12985ebfa0d2a444f7c8feca82fcbe2cb
Guzman Zugnoni <gooseiman@protonmail.com>
2024-01-13 20:59:21 -0300

    serve files

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,19 @@ use axum::{
     routing::get,
     Router,
 };
-use color_eyre::{eyre::WrapErr, Result};
+use color_eyre::{
+    eyre::{bail, ensure, ContextCompat, WrapErr},
+    Result,
+};
 use parking_lot::RwLock;
 use std::{
     env,
+    io::SeekFrom,
     net::SocketAddr,
     path::{Path, PathBuf},
     sync::Arc,
 };
+use tokio::io::AsyncSeekExt;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -233,12 +238,49 @@ fn content_type_from_extension(ext: Option<&str>) -> &str {
     }
 }
 
+fn parse_ranges(range: &str) -> Result<Vec<(Option<u64>, Option<u64>)>> {
+    let mut vals = range.split('=');
+    ensure!(
+        vals.next().context("Missing range unit")? == "bytes",
+        "Range unit was not bytes"
+    );
+    let range_str = vals.next().context("Missing range values")?;
+    let mut res = vec![];
+    for range in range_str.split(',') {
+        let mut it = range.split('-');
+        let v1 = it.next().wrap_err("Missing any value for range")?;
+        let v1 = if v1.is_empty() {
+            None
+        } else {
+            Some(
+                v1.parse::<u64>()
+                    .wrap_err_with(|| format!("Invalid value for range: {v1}"))?,
+            )
+        };
+
+        let v2 = it.next().wrap_err("Missing `-` in range")?;
+        let v2 = if v2.is_empty() {
+            None
+        } else {
+            Some(
+                v2.parse::<u64>()
+                    .wrap_err_with(|| format!("Invalid value for range: {v2}"))?,
+            )
+        };
+        if it.next().is_some() {
+            bail!("Excess range in {range}");
+        }
+        res.push((v1, v2));
+    }
+    Ok(res)
+}
+
 async fn dl_path(
     extract::Path(path): extract::Path<PathBuf>,
     State(state): State<AppState>,
     headers: HeaderMap,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    info!("Downloding [{p}]", p = path.to_string_lossy());
+    info!("Downloading [{p}]", p = path.to_string_lossy());
     let data_dir = &state.data_dir;
     let file_path = {
         let mut p = data_dir.to_path_buf();
@@ -255,23 +297,79 @@ async fn dl_path(
             format!("TODO: Cannot download folders yet: requested {path:?}"),
         ));
     }
-    let len = metadata.len();
+    let file_len = metadata.len();
     let ext = path.extension().map(|s| s.to_str().unwrap());
-    let ext = content_type_from_extension(ext);
-    let file = tokio::fs::File::open(file_path)
-        .await
-        .map_err(|e| (StatusCode::NOT_FOUND, e.to_string()))?;
-    let stream = tokio_util::io::ReaderStream::new(file);
-    let stream = axum::body::Body::from_stream(stream);
+    let content_type = content_type_from_extension(ext);
+    if headers.contains_key("Range") {
+        let ranges = headers
+            .get("Range")
+            .unwrap()
+            .to_str()
+            .map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))?;
+        let ranges = parse_ranges(ranges).map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))?;
+        info!("Requested ranges: {ranges:?}");
+        if ranges.len() != 1 {
+            return Err((
+                StatusCode::RANGE_NOT_SATISFIABLE,
+                "Do not support multiple ranges in Range request".to_string(),
+            ));
+        }
+        if ranges.is_empty() {
+            return Err((StatusCode::RANGE_NOT_SATISFIABLE, "You shouldn't send a range request without an actual range. That's bad for the environment".to_string()));
+        }
+        let start = ranges[0]
+            .0
+            .context("Range without starting range not supported")
+            .map_err(|e| (StatusCode::RANGE_NOT_SATISFIABLE, e.to_string()))?;
 
-    Response::builder()
-        .header("Accept-Ranges", "bytes")
-        .header("Content-Length", len)
-        .header("Content-Type", ext)
-        .header(
-            "Content-Disposition",
-            format!("attachment; filename=\"{p}\"", p = path.to_string_lossy()),
-        )
-        .body(stream)
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+        let mut file = tokio::fs::File::open(file_path)
+            .await
+            .map_err(|e| (StatusCode::NOT_FOUND, e.to_string()))?;
+        if start > file_len {
+            return Err((
+                StatusCode::RANGE_NOT_SATISFIABLE,
+                "The range start was past the end of the file".to_string(),
+            ));
+        }
+        file.seek(SeekFrom::Current(start.try_into().unwrap()))
+            .await
+            .map_err(|e| {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Failed to seek file: {e}"),
+                )
+            })?;
+        let stream = tokio_util::io::ReaderStream::new(file);
+        let stream = axum::body::Body::from_stream(stream);
+        let sent_len = file_len - start;
+        let end = file_len - 1;
+        Response::builder()
+            .status(206)
+            .header("Content-Range", format!("bytes {start}-{end}/{file_len}"))
+            .header("Content-Length", sent_len)
+            .header("Content-Type", content_type)
+            .header(
+                "Content-Disposition",
+                format!("attachment; filename=\"{p}\"", p = path.to_string_lossy()),
+            )
+            .body(stream)
+            .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+    } else {
+        let file = tokio::fs::File::open(file_path)
+            .await
+            .map_err(|e| (StatusCode::NOT_FOUND, e.to_string()))?;
+        let stream = tokio_util::io::ReaderStream::new(file);
+        let stream = axum::body::Body::from_stream(stream);
+
+        Response::builder()
+            .header("Accept-Ranges", "bytes")
+            .header("Content-Length", file_len)
+            .header("Content-Type", content_type)
+            .header(
+                "Content-Disposition",
+                format!("attachment; filename=\"{p}\"", p = path.to_string_lossy()),
+            )
+            .body(stream)
+            .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+    }
 }
